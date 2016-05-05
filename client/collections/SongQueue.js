9210c5491b4e346d5aa3ab2350bcc9388e216c0b
aphavichitr <andrew.phavichitr@gmail.com>
2016-05-04 22:13:37 -0700

    Complete SongQueue and SongModel

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,30 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    //console.log(this);
+    this.on('add', function () {
+      if (this.length === 1) {
+        this.playFirst();
+      }     
+    });
+
+    this.on('ended', function () {
+      this.shift();
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function () {
+      this.shift();
+    });
+ 
+  },
+
+  playFirst: function() {
+    //this.at(0).play();
+    //_.first(this.models).play();
+    this.models[0].play();
   }
 
-});
+}); 
